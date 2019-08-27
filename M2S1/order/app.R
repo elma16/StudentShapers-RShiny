130eccabef666982fdfbdff7e38a084115329b9a
elliott98 <elliott.macneil17@imperial.ac.uk>
2019-08-27 15:52:40 +0100

    Cleaning up folders

--- M2S1/order/app.R
+++ M2S1/order/app.R
@@ -1,113 +1,85 @@
 library(shiny)
 
-
-#get a random sample
-#create order statistics
-
 #---------------------- 
 ui <- bootstrapPage(
-   #includeHTML("navbar.html"),
-   headerPanel("Order Statistics"),
-   sidebarPanel(
-      selectInput("illustration", "Choose Distribution:",choices = c("Binomial" = "binom", "Poisson" = "poi","Normal"="norm")),
-      numericInput("rstat", "rth order statistics:", 1, min = 1, max = 100),
-      
-      conditionalPanel(
-         condition="input.illustration==\"binom\"",
-         sliderInput("binom.n","Number of attempts",min=1,max=100,value=10),
-         sliderInput("binom.p","Success probability p",min=0,max=1,value=0.5),
-         sliderInput("binom.size","Size",min = 1,max = 10,value=1),
-         sliderInput("binom.seed","Seed for Random Numbers",min=1,max=5000,value=1231,step=1)
-      ),
-      
-      conditionalPanel(
-         condition="input.illustration==\"poi\"",
-         sliderInput("poi.n","Number of attempts",min=1,max=100,value=10),
-         sliderInput("poi.lam",withMathJax("\\(\\lambda\\)"),min=0.1,max=10,value=1,step=0.1)
-      ),
-      
-      conditionalPanel(
-         condition="input.illustration==\"norm\"",
-         sliderInput("norm.n",withMathJax("\\(n\\)"),min=1,max=100,value=1,step=1),
-         sliderInput("norm.mean","\\(\\mu\\)",min=0.1,max=10,value=1,step=0.1),
-         sliderInput("norm.sd","\\(\\sigma\\)",min=0.1,max=10,value=1,step=0.1)
-      ),
-      
-      withMathJax(helpText("F_Y(y) = sum_{j = r}^n"))
-      #helpText("MLE:  \\(\\hat{p}=\\frac{1}{n}\\sum_{i=1}^n X_i\\).")
-   ),
-   mainPanel(verbatimTextOutput("observations"),plotOutput("likelihood"))
-   #includeHTML("mycode.html")
+    headerPanel("Order Statistics"),
+    sidebarPanel(
+        selectInput("illustration", "Choose Distribution:",choices = c("Binomial" = "binom", "Poisson" = "poi","Normal"="norm")),
+        numericInput("rstat", "rth order statistics:", 1, min = 1, max = 100),
+        
+        conditionalPanel(
+            condition="input.illustration==\"binom\"",
+            sliderInput("binom.n","Number of attempts",min=1,max=100,value=10),
+            sliderInput("binom.p","Success probability p",min=0,max=1,value=0.5),
+            sliderInput("binom.size","Size",min = 1,max = 10,value=1),
+            sliderInput("binom.seed","Seed for Random Numbers",min=1,max=5000,value=1231,step=1)
+        ),
+        
+        conditionalPanel(
+            condition="input.illustration==\"poi\"",
+            sliderInput("poi.n","Number of attempts",min=1,max=100,value=10),
+            sliderInput("poi.lam",withMathJax("\\(\\lambda\\)"),min=0.1,max=10,value=1,step=0.1)
+        ),
+        
+        conditionalPanel(
+            condition="input.illustration==\"norm\"",
+            sliderInput("norm.n",withMathJax("\\(n\\)"),min=1,max=100,value=1,step=1),
+            sliderInput("norm.mean","\\(\\mu\\)",min=0.1,max=10,value=1,step=0.1),
+            sliderInput("norm.sd","\\(\\sigma\\)",min=0.1,max=10,value=1,step=0.1)
+        )
+        
+    ),
+    mainPanel(verbatimTextOutput("observations"),plotOutput("likelihood"))
 )
 
 
 #------------------------------------
 server <- function(input, output) {
-   #observations for the order
-   output$observations <- renderText({
-      
-      if(input$illustration=="binom"){
-      n <- input$binom.n
-      p <- input$binom.p
-      size <- input$binom.size
-      set.seed(input$binom.seed)
-      X <- rbinom(n,size,p)
-      Xsorted <- sort(X,decreasing = FALSE)
-      paste("Observations:",
-            paste(X,collapse=", "),
-            #paste("\\hat{p}=",sum(X)/input$n,sep=""),
-            "Ordered Observations:",paste(Xsorted, collapse =", "),
-            sep="\n")
-      }
-      
-      if(input$illustration=="poi"){
-      n <- input$poi.n
-      lam <- input$poi.lam
-      Y <- rpois(n,lam)
-      Ysorted <- sort(Y,decreasing = FALSE)
-      paste("Observations:",
-            paste(Y,collapse=", "),
-            #paste("\\hat{p}=",sum(X)/input$n,sep=""),
-            "Ordered Observations:",paste(Ysorted, collapse =", "),
-            sep="\n")
-      }
-      
-      if(input$illustration=="norm"){
-      n <- input$norm.n
-      mean <- input$norm.mean
-      sd <- input$norm.sd
-      Z <- rnorm(m,mean,sd)
-      Zsorted <- sort(Z,decreasing = FALSE)
-      paste("Observations:",
-            paste(Z,collapse=", "),
-            #paste("\\hat{p}=",sum(X)/input$n,sep=""),
-            "Ordered Observations:",paste(Zsorted, collapse =", "),
-            sep="\n")
-      }
-   })
-   #output$likelihood <-   renderPlot({
-   #  
-   #    if(input$illustration=="binom"){
-   #    set.seed(input$seed)
-   #    X <- rbinom(n,size=1,p)
-   #    #Y <- sum(choose(n,j),X^j(1-X)^j)
-   #    p <- seq(0,1,by=0.005)
-   #    plot()
-   #    plot(p,p^sum(X)*(1-p)^(input$n-sum(X)),xlab="p",ylab="L(p)",main="Likelihood function",type="l")
-   #    lines(rep(sum(X)/input$n,2),c(0,1000),col="red")
-   #    }
-   #    
-   #    if(input$illustration=="poi"){
-   #       
-   #    }
-   #    
-   #    if(input$illustration=="norm"){
-   #       
-   #    }
-   #    
-   # })
+    output$observations <- renderText({
+        
+        if(input$illustration=="binom"){
+            n <- input$binom.n
+            p <- input$binom.p
+            size <- input$binom.size
+            set.seed(input$binom.seed)
+            X <- rbinom(n,size,p)
+            Xsorted <- sort(X,decreasing = FALSE)
+            Out <- paste("Observations:",
+                         paste(X,collapse=", "),
+                         "Ordered Observations:",paste(Xsorted, collapse =", "),
+                         sep="\n")
+        }
+        
+        if(input$illustration=="poi"){
+            n <- input$poi.n
+            lam <- input$poi.lam
+            Y <- rpois(n,lam)
+            Ysorted <- sort(Y,decreasing = FALSE)
+            Out <- paste("Observations:",
+                         paste(Y,collapse=", "),
+                         "Ordered Observations:",paste(Ysorted, collapse =", "),
+                         sep="\n")
+        }
+        
+        if(input$illustration=="norm"){
+            n <- input$norm.n
+            mean <- input$norm.mean
+            sd <- input$norm.sd
+            Z <- rnorm(n,mean,sd)
+            Zsorted <- sort(Z,decreasing = FALSE)
+            Out <- paste("Observations:",
+                         paste(Z,collapse=", "),
+                         "Ordered Observations:",paste(Zsorted, collapse =", "),
+                         sep="\n")
+        }
+        Out
+    })
+    
+    output$likelihood <- renderPlot({
+        
+        
+    })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
 
+shinyApp(ui = ui, server = server)
--- Shiny-toys/randomwalks/app.R
+++ Shiny-toys/randomwalks/app.R
@@ -1,45 +1,80 @@
-#this is the webapp version of a script i wrote for constructing random walks
+#-----------------------------------------------
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+#-----------------------------------------------
 
-    # Application title
-    titlePanel("Random Walks"),
+ui <- navbarPage("Random Walks",
+                   #don't quite understand what the point of this is
+                   #includeHTML("navbar.html"),
+                   
+                   #changes the theme of the webpage
+                   theme = shinythemes::shinytheme("cerulean"),
+                   
+                   #title
+                   #headerPanel("Illustrations in M2S1"),
+                 
+                 fluidRow(
+                     column(3,
+                            # Slider input 
+                            selectInput("illustration", "Choose Walk:",
+                                        #the options for the distributions to choose between
+                                        choices = c("1d" = "1d", "2d" = "2d","angle" = "angle")),
+                            #1d
+                            conditionalPanel(
+                                condition="input.illustration==\"1d\"",
+                                sliderInput("one.step", "Number of steps:", min = 10, max = 500, value = 100),
+                                sliderInput("one.jump", "Size of jump",min = 1, max = 10, value = 1)
+                            ),
+                            #lattice
+                            conditionalPanel(
+                                condition="input.illustration==\"2d\"",
+                                sliderInput("two.N",withMathJax(helpText("Mean : \\(\\mu\\)")),min=10,max=1000000,value=10000,step=1)
+                            ),
+                            #angled
+                            conditionalPanel(
+                                condition="input.illustration==\"angle\"",
+                                sliderInput("angle.step", "Number of steps:", min = 10, max = 500, value = 100),
+                                sliderInput("angle.jump", "Size of jump",min = 1, max = 10, value = 1)
+                            ),
+                            #links to all the code
+                            helpText(a(href = "https://github.com/elma16/StudentShapers-RShiny/blob/master/M2S1/Distributions/app.R", target = "_blank", "View code")),
+                            helpText(a(href = "https://github.com/elma16/StudentShapers-RShiny", target = "_blank", "Check out other apps")),
+                            helpText(a(href = "https://openintro.org", target = "_blank", "Learn more for free!"))
+                            
+                     ),
+                     
+                     column(3, offset=1,
+                            
+                            # Maths equations
+                            withMathJax(),
+                            uiOutput('tdisteq')
+                            
+                     )
+                     
+                 ),
+                   
+                            
+                            
+                            #text outputs to accompany the plots 
+                            mainPanel(
+                                plotOutput("cdf")
+                            )
+                            
+   
+                   
+                   #includeHTML("mycode.html")
+)
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("illustration","Choose walk:",
-                        choices = c("1D" = "1d", "2D" = "2d","Angled" = "angle"))
-            ) ,           
-        # conditionalPanel(
-        #     condition="input.illustration==\"1d\"",
-        #     sliderInput("one.step", "Number of steps:", min = 10, max = 500, value = 100),
-        #     sliderInput("one.jump", "Size of jump",min = 1, max = 10, value = 1)
-        #     ),
-        # conditionalPanel(
-        #     condition="input.illustration==\"2d\"",
-        #     sliderInput("lattice.step", "Number of steps:", min = 10, max = 500, value = 100),
-        #     sliderInput("lattice.jump", "Size of jump",min = 1, max = 10, value = 1)
-        # ),
-        # conditionalPanel(
-        #     condition="input.illustration==\"angle\"",
-        #     sliderInput("angle.step", "Number of steps:", min = 10, max = 500, value = 100),
-        #     sliderInput("angle.jump", "Size of jump",min = 1, max = 10, value = 1)
-        # )
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-# Define server logic required to draw a histogram
+
+
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
+    
+    #plotting the random walks
+    #one dimensional
+    output$cdf <- renderPlot({
+        if (input$illustration=="1d"){
             # generate bins based on input$bins from ui.R
             step <- input$one.step
             jump <- input$one.jump
@@ -63,77 +98,82 @@ server <- function(input, output) {
             # print on the plot
             label <- paste("distance=", signif(r, 3)) 
             text(max(x), max(y), labels=label, pos=2)
+        }
+        #lattice path
+        if (input$illustration=="2d"){
+            m <- input$two.N
+            RW2D<-function(N)
+            {
+                i<-0
+                xdir<-0
+                ydir<-0
+                xpos<-vector()
+                xpos[1]<-xdir
+                ypos<-vector()
+                ypos[1]<-ydir
+                for (i in 1:N-1)
+                {
+                    r<-runif(1)
+                    if(r<=0.25) {xdir<-xdir+1}
+                    if(r>0.25 && r<=0.5) {xdir<-xdir-1}
+                    if(r>0.5 && r<=0.75) {ydir<-ydir +1}
+                    if(r>0.75) {ydir<-ydir-1}
+                    xpos[i+1]<-xdir
+                    ypos[i+1]<-ydir
+                }
+                return(cbind(xpos,ypos))
+            }
+            rw<-RW2D(m)
+            
+            xmin<-min(rw[,1])
+            xmax<-max(rw[,1])
+            ymin<-min(rw[,2])
+            ymax<-max(rw[,2])
+            
+            plot(rw[,1],rw[,2],type="l",xlab="x",ylab="y",main="Random Walk Simulation In Two Dimensions",col="green4",xlim=range(xmin:xmax),ylim=range(ymin:ymax))
+            
+            end<-cbind(rw[m,1],rw[m,2])
+            start<-cbind(0,0)
+            
+            points(start,pch=4,col="red")
+            points(end,pch=4,col="red")
+            
+        }
         
-        # if (input$illustration=="2d"){
-        #     step <- input$lattice.step
-        #     jump <- input$lattice.jump
-        #     RW2D<-function(N)
-        #     {
-        #         i<-0
-        #         xdir<-0
-        #         ydir<-0
-        #         xpos<-vector()
-        #         xpos[1]<-xdir
-        #         ypos<-vector()
-        #         ypos[1]<-ydir
-        #         for (i in 1:N-1)
-        #         {
-        #             r<-runif(1)
-        #             if(r<=0.25) {xdir<-xdir+1}
-        #             if(r>0.25 && r<=0.5) {xdir<-xdir-1}
-        #             if(r>0.5 && r<=0.75) {ydir<-ydir +1}
-        #             if(r>0.75) {ydir<-ydir-1}
-        #             xpos[i+1]<-xdir
-        #             ypos[i+1]<-ydir
-        #         }
-        #         return(cbind(xpos,ypos))
-        #     }
-        #     rw<-RW2D(10000)
-        #     
-        #     xmin<-min(rw[,1])
-        #     xmax<-max(rw[,1])
-        #     ymin<-min(rw[,2])
-        #     ymax<-max(rw[,2])
-        #     
-        #     plot(rw[,1],rw[,2],type="l",xlab="x",ylab="y",main="Random Walk Simulation In Two Dimensions",col="green4",xlim=range(xmin:xmax),ylim=range(ymin:ymax))
-        #     
-        #     end<-cbind(rw[10000,1],rw[10000,2])
-        #     start<-cbind(0,0)
-        #     
-        #     points(start,pch=4,col="red")
-        #     points(end,pch=4,col="red")
-        #     
-        # }
-        # 
-        # if (input$illustration=="angle"){
-        #     step <- input$angle.step
-        #     jump <- input$angle.jump
-        #     # generate n.step random directions (angles)
-        #     theta <- runif(n.step, 0, 2*pi)
-        #     # compute the x and y step sizes
-        #     dx <- n.jump * cos(theta) 
-        #     dy <- n.jump * sin(theta)
-        #     # compute the cumulative x and y positions
-        #     x <- c(0, cumsum(dx)) 
-        #     y <- c(0, cumsum(dy))
-        #     # plot the walk
-        #     plot(x, y, type="l", bty="n", col="red")
-        #     # plot the start and end points
-        #     points(x[1], y[1], pch=16, cex=1.5) 
-        #     points(x[n.step+1], y[n.step+1], pch=16,cex=1.5)
-        #     # join the start and end points
-        #     lines(x[c(1, n.step+1)], y[c(1, n.step+1)], lwd=3)
-        #     # compute the distance from start to end
-        #     r <- sqrt(x[n.step+1]^2 + y[n.step+1]^2)
-        #     # print on the plot
-        #     label <- paste("distance=", signif(r, 3)) 
-        #     text(max(x), max(y), labels=label, pos=2)
-        #     
-        # }
-        
-        
+        if (input$illustration=="angle"){
+            step <- input$angle.step
+            jump <- input$angle.jump
+            # generate n.step random directions (angles)
+            theta <- runif(step, 0, 2*pi) 
+            # compute the x and y step sizes
+            dx <- jump * cos(theta) 
+            dy <- jump * sin(theta)
+            # compute the cumulative x and y positions
+            x <- c(0, cumsum(dx)) 
+            y <- c(0, cumsum(dy))
+            # plot the walk
+            plot(x, y, type="l", bty="n", col="red")
+            
+            # plot the start and end points
+            points(x[1], y[1], pch=16, cex=1.5) 
+            points(x[step+1], y[step+1], pch=16,cex=1.5)
+            # join the start and end points
+            lines(x[c(1, step+1)], y[c(1, step+1)], lwd=3)
+            # compute the distance from start to end
+            r <- sqrt(x[step+1]^2 + y[step+1]^2)
+            # print on the plot
+            label <- paste("distance=", signif(r, 3)) 
+            text(max(x), max(y), labels=label, pos=2)
+        }
     })
+
+    
+
+        
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
+
